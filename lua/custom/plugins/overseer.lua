f04c406b98720d877d3113b1d896e5aa6126aeb0
neko <neko@neko-MacBook-Pro.local>
2025-01-15 19:16:26 +0800

    setup overseer

--- lua/custom/plugins/overseer.lua
+++ lua/custom/plugins/overseer.lua
@@ -2,17 +2,7 @@ return {
   'stevearc/overseer.nvim',
   opts = {},
   config = function()
-    local overseer = require 'overseer'
-    overseer.register_template {
-      name = 'go run',
-      builder = function(params)
-        return {
-          cmd = { 'go' },
-          args = { 'run', '.' },
-        }
-      end,
-    }
-    overseer.setup()
+    require('overseer').setup()
   end,
   keys = {
     { '<leader>tt', '<cmd>OverseerToggle<cr>', desc = '[T]oggle [T]ask' },
